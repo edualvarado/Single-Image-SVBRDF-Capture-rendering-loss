cb5fb4653718f09a9ff0e9cd24449414325bbb51
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-08-03 18:44:19 +0200

    CHANGED: Re-structure project

--- runExampleTest.sh
+++ runExampleTest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Please download the checkpoint before running
-python3 material_net.py --input_dir inputExamples/ --mode eval --output_dir examples_outputs --checkpoint . --imageFormat png --scale_size 256 --batch_size 1
+python material_net.py --input_dir Inputs/inputTest/scaled/ --mode eval --output_dir Outputs/test1 --checkpoint Model/ --imageFormat jpg --scale_size 256 --batch_size 1
